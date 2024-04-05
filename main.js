bdb17ca4501a65f6fc0109ffc8bd1c96169e2f58
MahmudurNishat <rajbiroy23@gmail.com>
2024-04-05 11:40:19 +0600

    first

---
+++ main.js
@@ -0,0 +1,53 @@
+////
+
+let a = 30;
+let b = 40;
+
+let sum = a + b;
+console.log("the sum is ", sum); // the sum is  70
+
+let X = "30";
+let Y = 40;
+
+let sums = X + Y;
+console.log("the sum is ", sums);
+//  the sum is  3040
+// 1st e string paise porer ta ke string banaite pare nai , tai se tar pase bose gese ,
+// keal korbe 3040 sada color mane pura tai ekon string , tobe se sudu + er khetre erokom ta kore ,
+// baki operator er janno ta kore na
+
+let p = "30";
+let q = 40;
+
+let minus = p - q; // let minus = p - q; // ekane equal holo operator , minus,p,q holo operand ,
+console.log("the minus is ", minus); // the minus is  -10
+
+
+let u = 5;
+let i = 2;
+console.log("the exponential is", u ** i);// the exponential is 25
+console.log("the exponential is", u ** 2 ); // the exponential is 25
+
+
+// know about Arithmatic operation like addition , substraction , multiplication , divide ,            Modulas (bagses) from w3 school ..
+
+// lets talk about pre-increment  // we will discuss post-increment in loop 
+
+let o = 5;
+let increment = ++o // pre incre // aage thekle pre// ekta kore value se add korbe // pore thakle post //
+console.log("The increment is ", increment); // The increment is  6
+
+let r = 5;
+let increments = ++r 
+console.log("The increment is ", ++increments); // The increment is  7 // ek ek kore varce 
+
+let s = 5;
+ ++s ;
+ let incrementss = s // s er value 6 hoice seta incrementss er bitore save hoice
+console.log("The increment is ", incrementss); // The increment is  6//
+
+// same as pre-decrement /// 
+
+let Q = 11;
+let decrement = --Q  // value 10
+console.log("The decrement is ", --decrement);// value 9 // The decrement  is  9  // ek ek kore komce
